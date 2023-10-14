a3f99e0b83c6be611da49589420ee0e212557327
calofonseca <calof@isep.ipp.pt>
2023-10-14 02:52:30 +0100

    Updates

--- install/IR_assignment1_controller/lib/python3.10/site-packages/IR_assignment1_controller/wall_follower.py
+++ install/IR_assignment1_controller/lib/python3.10/site-packages/IR_assignment1_controller/wall_follower.py
@@ -4,7 +4,6 @@ from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 
-
 class WallFollowingNode(Node):
     def __init__(self):
         super().__init__('wall_following_node')
@@ -21,106 +20,161 @@ class WallFollowingNode(Node):
         )
         self.error_sum = 0
         self.prev_error = 0
-        self.Kp = 1.0
-        self.Ki = 0.1
-        self.Kd = 0.01
-        self.max_speed = 1.0
-        self.min_speed = 0.5
+        self.range = 2.0
+        self.Kp = 0.1
+        self.Ki = 0.0
+        self.Kd = 0.0
+        self.distance = 1
+        self.max_windup = 10
+        self.error_log = []
+        self.following_side = None  # 'left' or 'right'
         self.get_logger().info("Wall Follower Node has been started")
 
     def laser_callback(self, msg):
-        # Your existing laser_callback code...
-        
-        # Conditions and states
-        wall_detected = min(msg.ranges[320:640]) < 3.5  # example condition
-        stopping_condition_detected = self.check_stopping_condition(msg)  # Implement this method
-        
-        if stopping_condition_detected:
-            self.state = "stopping"
-        elif wall_detected:
-            self.state = "following_wall"
-        else:
-            self.state = "wandering"
 
-        # Decision-making and executing behaviors
-        if self.state == "wandering":
+        regions_ = {
+            'bright': min(min(msg.ranges[0:102]), self.range),    # Back-right
+            'right': min(min(msg.ranges[102:290]), self.range),   # Right
+            'fright': min(min(msg.ranges[290:330]), self.range),  # Front-right
+            'front': min(min(msg.ranges[330:390]), self.range),   # Front
+            'fleft': min(min(msg.ranges[390:430]), self.range),   # Front-left
+            'left': min(min(msg.ranges[430:618]), self.range),    # Left
+            'bleft': min(min(msg.ranges[618:720]), self.range),   # Back-left
+            'right_early': sum([i for i in msg.ranges[190:220] if i != float('inf')]) / len([i for i in msg.ranges[190:220] if i != float('inf')]) if [i for i in msg.ranges[190:220] if i != float('inf')] else float('10'),  # Average for 102-216 degrees
+            'right_late': sum([i for i in msg.ranges[140:170] if i != float('inf')]) / len([i for i in msg.ranges[140:170] if i != float('inf')]) if [i for i in msg.ranges[140:170] if i != float('inf')] else float('10'),   # Average for 216-320 degrees
+            'left_early': sum([i for i in msg.ranges[400:504] if i != float('inf')]) / len([i for i in msg.ranges[400:504] if i != float('inf')]) if [i for i in msg.ranges[400:504] if i != float('inf')] else float('1e10'),   # Average for 400-504 degrees
+            'left_late': sum([i for i in msg.ranges[504:618] if i != float('inf')]) / len([i for i in msg.ranges[504:618] if i != float('inf')]) if [i for i in msg.ranges[504:618] if i != float('inf')] else float('1e10'),    # Average for 504-618 degrees
+        }
+
+        self.get_logger().info(str(regions_))
+
+        if self.end_condition(regions_):
+            self.end_at_goal()
+        elif self.is_wall_detected(regions_):
+            if self.following_side is None:
+                self.choose_following_side(regions_)  # Choose side when first detected a wall
+            if self.following_side is not None:
+                self.get_logger().info(f"Following Wall on " + str(self.following_side))
+                self.follow_wall(regions_)
+        else:
+            self.get_logger().info(f"Wander")
             self.wander()
-        elif self.state == "following_wall":
-            self.follow_wall(msg)
-        elif self.state == "stopping":
-            self.stop_at_goal()
+
+    def choose_following_side(self, regions):
+        # Choose the side to follow based on certain logic. 
+        # For example, choose the side that has the closest wall when first detected.
+        if regions['left'] < regions['right']:
+            self.following_side = 'left'
+            self.get_logger().info(f"Chosen to follow: {self.following_side} wall")
+        elif regions['left'] > regions['right']: 
+            self.following_side = 'right'
+            self.get_logger().info(f"Chosen to follow: {self.following_side} wall")
+        
 
     def wander(self):
-        # Implement logic to wander around
-        self.send_command(0.1, 0.0)
-        return
-    
-    def follow_wall(self, msg):
-        self.get_logger().info("Received: " + str(len(msg.ranges)))
+        # Logic to wander around
+        self.send_command(0.5, 0.0)
 
-        # Assuming the LIDAR data ranges array is 360 degrees
-        # Extract right and left side measurements
-        right_distance = min(msg.ranges[270:360])  # Adjust the angles as per your LIDAR orientation
-        self.get_logger().info("Right: " + str(right_distance))
-        left_distance = min(msg.ranges[0:90])      # Adjust the angles as per your LIDAR orientation
-        self.get_logger().info("Left: " + str(left_distance))
-        desired_distance = 2.0  # Desired distance from the wall
-    
-        # Check which side the wall is closer to
-        if right_distance < left_distance:
-            error = desired_distance - right_distance
-            self.get_logger().info("Following wall on the right")
-        else:
-            error = desired_distance - left_distance
-            self.get_logger().info("Following wall on the left")
+    def is_wall_detected(self, regions):
+        return regions['fright'] < self.range or regions['fleft'] < self.range or regions['right'] < self.range or regions['left'] < self.range
 
-        # Proportional control
-        P = self.Kp * error
 
-        # Integral control
-        self.error_sum += error
-        I = self.Ki * self.error_sum
+    def end_condition(self, regions):
+        # You might determine a specific condition based on region readings
+        # For instance, when all frontal regions have no obstacle:
+        #return regions['bright'] > self.range and regions['right'] > self.range and regions['fright'] > self.range and regions['front'] > self.range and regions['fleft'] > self.range and regions['bleft'] > self.range and regions['left'] < self.distance
+        return False
 
-        # Derivative control
-        D = self.Kd * (error - self.prev_error)
+    def follow_wall(self, regions):
+        # Define the early and late sections for both sides
+        early_avg= regions['right_early'] if self.following_side == 'right' else regions['left_early']
+        late_avg = regions['right_late'] if self.following_side == 'right' else regions['left_late']
+
+        # The 'angle' difference will be the difference in these averages
+        self.get_logger().info(f"early avg: " + str(early_avg))
+        self.get_logger().info(f"late avg: " + str(late_avg))
+
+        angle_diff = early_avg - late_avg # on right if positive is heading out of the wall and negative is going to the wall 
+
+        self.get_logger().info(f"Difference: " + str(angle_diff))
+
+        # Control logic based on the angle difference
+        # The idea is if angle_diff is positive, it means robot is getting farther from the wall 
+        # on the 'late' section and needs to turn towards the wall. Negative would mean the opposite.
+        if self.following_side == "right":
+            angle_diff = -angle_diff  # reverse the sign for right side
+        
+        # Use angle_diff as the error for PID
+        error = angle_diff
+
+        # Rest of the PID logic remains the same
+        self.error_log.append(error)
+        self.error_sum = min(max(self.error_sum + error, -self.max_windup), self.max_windup)
+
+        control_output = (
+            self.Kp * error +
+            self.Ki * self.error_sum +
+            self.Kd * (error - self.prev_error)
+        )
+
+        speed = 0.5
+
         self.prev_error = error
 
-        control_output = P + I + D
+        self.get_logger().info(f"Control Output: " + str(control_output))
+            
+        # Sending control commands
+        self.send_command(speed, control_output)
 
-        # Variable speed control
-        speed = self.max_speed - abs(error) * 0.1  # Example speed control formula
-        speed = max(min(speed, self.max_speed), self.min_speed)  # Clamp speed to min/max range
+    #def follow_wall(self, regions):
+    #    # As an example, let’s say you want to follow a wall on the right. 1.23-1 = 0.23
+    #    error = regions[self.following_side] - self.distance
+    #    self.get_logger().info(f"Error: " + str(error))
 
-        # Decide direction based on the side of the wall
-        if right_distance < left_distance:
-            # If following right wall, control_output remains same
-            pass
-        else:
-            # If following left wall, invert the direction of control_output
-            control_output = -control_output
-    
-        self.send_command(0.1, control_output)
-        return
-    
-    def stop_at_goal(self):
-        # Implement logic to stop or change behavior at the goal
-        self.send_command(0.0, 0.0)
-        return
-    
-    def check_stopping_condition(self, msg):
-        # Implement logic to check for stopping condition based on sensor data
-        # Example: If LIDAR data show a particular pattern (e.g., a sharp tip)
-        # ...
-        return False  # Replace with actual logic       
+    #    if self.following_side == "right":
+    #        error = -error
+
+    #    self.error_log.append(error)
+
+    #    # Updating error for next iteration
+    #    self.error_sum = min(max(self.error_sum + error, -self.max_windup), self.max_windup)
+    #    # PID Control
+    #    control_output = (
+    #        self.Kp * error +
+    #        self.Ki * self.error_sum +
+    #        self.Kd * (error - self.prev_error)
+    #    )
+
+    #    # Limiting speed and control output
+    #    speed = min(max(0.5, 1.2 - abs(error)), 1.2)  # Clip speed between 0.5 and 1.5
+    #    speed = 0.5
+    #    self.get_logger().info(f"Control Out: " + str(control_output))
+        #control_output = min(max(control_output, -1.0), 1.0)  # Clip control output between -1 and 1
+        #self.get_logger().info(f"Control Output: " + str(control_output))
+
         
+    #    self.prev_error = error
+        
+        # Sending control commands
+    #    self.send_command(speed, control_output)
+
+
+    def end_at_goal(self):
+        # Logic to stop or change behavior at the goal
+        self.send_command(0.0, 0.0)
+        self.save_errors_to_file()
+
     def send_command(self, speed, control_output):
         # Create and publish a Twist message to control the robot
-        self.get_logger().info("Trying to send: " + str(speed) + " control" + str(control_output))
         twist_msg = Twist()
-        twist_msg.linear.x = speed  # Forward speed
-        twist_msg.angular.z = control_output  # Angular speed
+        twist_msg.linear.x = float(speed)  # Forward speed
+        twist_msg.angular.z = float(control_output)  # Angular speed
         self.cmd_pub.publish(twist_msg)
-        self.get_logger().info("Published: " + str(twist_msg))
+    
+    def save_errors_to_file(self):
+        with open("error_log.txt", "a") as file:
+            for error in self.error_log:
+                file.write(f"{error}\n")
 
 def main():
     rclpy.init()
@@ -128,6 +182,7 @@ def main():
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
--- src/IR_assignment1_controller/launch/simulation_launch.py
+++ src/IR_assignment1_controller/launch/simulation_launch.py
@@ -1,28 +1,33 @@
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess, TimerAction
+from launch.actions import ExecuteProcess
 from launch_ros.actions import Node
-import os
 
 def generate_launch_description():
     return LaunchDescription([
+        # Launch Ignition Gazebo with the specified environment SDF
         ExecuteProcess(
-            cmd=['ign', 'gazebo', 'src/IR_assignment1_controller/worlds/envent.sdf'],
+            cmd=['./launch_ignition.sh', 'envent.sdf'],
             output='screen'
         ),
-        TimerAction(
-            period=5.0,  # Modify delay duration as needed
-            actions=[
-                Node(
-                    package='ros_gz_bridge',
-                    executable='parameter_bridge',
-                    arguments=[
-                        'cmd_vel@geometry_msgs/msg/Twist]ignition.msgs.Twist',
-                        'lidar@sensor_msgs/msg/LaserScan]ignition.msgs.LaserScan'
-                    ],
-                    output='screen'
-                )
-            ]
+        # Launch the parameter_bridge for cmd_vel
+        Node(
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
+            arguments=[
+                'cmd_vel@geometry_msgs/msg/Twist]ignition.msgs.Twist'
+            ],
+            output='screen'
         ),
+        # Launch the parameter_bridge for lidar
+        Node(
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
+            arguments=[
+                'lidar@sensor_msgs/msg/LaserScan[ignition.msgs.LaserScan'
+            ],
+            output='screen'
+        ),
+        # Launch the wall follower node
         Node(
             package='IR_assignment1_controller',
             executable='wall_follower',
